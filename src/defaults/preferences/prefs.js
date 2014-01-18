5661a5ec2c0bd211f8cae35d4d49ddf1904628b9
Opera Wang <Opera.Wang@gmail.com>
2014-01-18 21:19:18 +0800

    messages_number_limit default to 500

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -11,7 +11,7 @@ pref("extensions.awsome_auto_archive.idle_delay", 60);
 pref("extensions.awsome_auto_archive.start_next_delay", 3600);
 pref("extensions.awsome_auto_archive.start_exceed_delay", 120);
 pref("extensions.awsome_auto_archive.rule_timeout", 4000);
-pref("extensions.awsome_auto_archive.messages_number_limit", 0); // 0 to disable check
+pref("extensions.awsome_auto_archive.messages_number_limit", 500); // 0 to disable check
 pref("extensions.awsome_auto_archive.messages_size_limit", 0); // 0 to disable check, in M Bytes
 pref("extensions.awsome_auto_archive.update_statusbartext", true);
 pref("extensions.awsome_auto_archive.dry_run", false);
