2ae0ff66bb4eed5ae8dc6fc3dfa2ad344f893d12
Opera Wang <Opera.Wang@gmail.com>
2019-10-06 23:50:57 +0800

    replace install.rdf with manifest.json

--- src/release.sh
+++ src/release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 zip="zip -r"
-AllFiles="content locale defaults chrome.manifest manifest.json icon.png install.rdf bootstrap.js"
-version="$(grep "<em:version>" install.rdf | sed -e 's/.*<em:version>\(.*\)<\/em:version>.*/\1/g')"
+AllFiles="content locale defaults chrome.manifest manifest.json icon.png bootstrap.js"
+version="$(grep '"version"' manifest.json | sed -e 's/.:.*"\(.*\)".*/\1/g')"
 
 fileName="../beta/awsomeAutoArchive-${version}-tb.xpi"
 
